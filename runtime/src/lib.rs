4f2400f657ac782d0fc0ad075ed123febbf53850
Alain Brenzikofer <news@brenzi.ch>
2021-10-09 11:15:52 +0200

    export RpsCall

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -50,6 +50,8 @@ pub use frame_support::{
 };
 pub use pallet_balances::Call as BalancesCall;
 pub use pallet_timestamp::Call as TimestampCall;
+pub use pallet_rps::Call as RpsCall;
+
 #[cfg(any(feature = "std", test))]
 pub use sp_runtime::BuildStorage;
 pub use sp_runtime::{Perbill, Permill};
