7e070dbad310f911fefa7a842ff77e2cb155b6ee
haerdib <73821294+haerdib@users.noreply.github.com>
2021-12-19 13:13:14 +0100

    Add secp256k1 ecdsa recover (#42)
    * implement secp256k1_ecdsa_recover
    
    * fix sgx compilation error
    
    * reorder libsecp256k1 in .toml
    
    Co-authored-by: suinuj <junius@litentry.com>

--- substrate-sgx/sp-io/src/lib.rs
+++ substrate-sgx/sp-io/src/lib.rs
@@ -539,8 +539,18 @@ pub mod crypto {
 		sig: &[u8; 65],
 		msg: &[u8; 32],
 	) -> Result<[u8; 64], EcdsaVerifyError> {
-		warn!("crypto::secp256k1_ecdsa_recover unimplemented");
-		Ok([0; 64])
+		let rs = libsecp256k1::Signature::parse_standard_slice(&sig[0..64])
+			.map_err(|_| EcdsaVerifyError::BadRS)?;
+		let v = libsecp256k1::RecoveryId::parse(
+			if sig[64] > 26 { sig[64] - 27 } else { sig[64] } as u8
+		)
+		.map_err(|_| EcdsaVerifyError::BadV)?;
+		let pubkey = libsecp256k1::recover(&libsecp256k1::Message::parse(msg), &rs, &v)
+			.map_err(|_| EcdsaVerifyError::BadSignature)?;
+		let mut res = [0u8; 64];
+		res.copy_from_slice(&pubkey.serialize()[1..65]);
+
+		Ok(res)
 	}
 
 	pub fn secp256k1_ecdsa_recover_compressed(
