238016370b4a4aa274db6cc866c5c2ac80797123
Christian Langenbacher <clangenb+gh@protonmail.ch>
2022-08-02 21:22:03 +0200

    [runtime] fix compilation without evm feature

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -300,7 +300,7 @@ construct_runtime!(
 		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
 		Timestamp: pallet_timestamp::{Pallet, Call, Storage, Inherent},
 		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
-		TransactionPayment: pallet_transaction_payment::{Pallet, Storage},
+		TransactionPayment: pallet_transaction_payment::{Pallet, Storage, Event<T>},
 		Sudo: pallet_sudo::{Pallet, Call, Config<T>, Storage, Event<T>},
 		Parentchain: pallet_parentchain::{Pallet, Call, Storage},
 	}
